be32d4b92f8806d1b53775c5ecb28d36a8aa424f
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2020-10-16 13:11:39 +0200

    conexion a bd

--- JS/leerCSV.js
+++ JS/leerCSV.js
@@ -1,5 +1,17 @@
 /*https://www.kaggle.com/rounakbanik/the-movies-dataset?select=movies_metadata.csv*/
 
+/**
+ * Datu Basera konexioa egiteko objetua eta dena prestatzen dugu.
+ */
+include(/PHP/dbKonexioa.php);
+
+$konexioa = new konexioa("localhost", "lacuartapuerta", "root", "");
+
+$hostPDO = "mysql:host=$KONEXIOA->hostDB;dbname=$nombreDB;";
+$miPDO = new PDO($konexioa->hostPDO, $konexioa->usuarioDB, $konexioa->contrasenyaDB);
+/**
+ * ajax eta jquery erablita, .csv-ko eduki guztiak irakurtzen ditugu.
+ */
 $.ajax({
     url: 'movies_metadata.csv',
     dataType: 'text',
@@ -8,8 +20,14 @@ $.ajax({
 
   	
 function successFunction(data) {
+  /**
+   * Lerroak banatu
+   */
     var allRows = data.split(/\r?\n|\r/);
     var table = '<table>';
+    /**
+     * taulako izenburuak hartu
+     */
     for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
       if (singleRow === 0) {
         table += '<thead>';
@@ -17,16 +35,13 @@ function successFunction(data) {
       } else {
         table += '<tr>';
       }
+      /**
+       * taulan beste edukiak sartzen dira
+       */
       var rowCells = allRows[singleRow].split(',');
-      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
-        if (singleRow === 0) {
-          table += '<th>';
+      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) { 
           table += rowCells[rowCell];
-          table += '</th>';
-        } else {
-          table += '<td>';
           table += rowCells[rowCell];
-          table += '</td>';
         }
       }
       if (singleRow === 0) {
---
+++ PHP/dbKonexioa.php
@@ -0,0 +1,48 @@
+<?php
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ROOT ERABILTZAILEARI PASAHITZA ALDATZEKO XAMPPen SHELLean mysqladmin.exe -u root password WhateverPassword
+
+// .../XAMPP/phpMyAdmin/config.inc.php ALDATU --> $cfg['Servers'][$i]['password'] = 'WhateverPassword';
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//KONEXIOA EGITEKO DATUAK - SERBITZARIA - DATU BASEAREN IZENA - ERABILTZAILEA - PASAHITZA
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class konexioa{
+    var $hostDB;
+    var $nombreDB;
+    var $usuarioDB;
+    var $contrasenyaDB; 
+
+    function konexioa($hostDB,$nombreDB,$usuarioDB,$contrasenyaDB){ 
+        $this->hostDB = $hostDB; 
+        $this->nombreDB = $nombreDB; 
+        $this->usuarioDB = $usuarioDB; 
+        $this->contrasenyaDB =$contrasenyaDB;
+    }
+}
+
+
+
+
+// DB KONEXIOA
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// $hostPDO = "mysql:host=$hostDB;dbname=$nombreDB;";
+// $miPDO = new PDO($hostPDO, $usuarioDB, $contrasenyaDB);
+
+
+// Create TABLE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// $miConsulta = $miPDO->prepare('CREATE TABLE Datos (
+//     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//     Nombre VARCHAR(30) NOT NULL,
+//     Apellido VARCHAR(30) NOT NULL,
+//     email VARCHAR(50),
+//     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)
+//     ');
+
+// $miConsulta->execute();
+
+?>
