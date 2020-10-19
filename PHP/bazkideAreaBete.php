32ae8b87a7ed688b6b32ca74d35b920dcdff5a7c
oarranz <oihanefpweb@gmail.com>
2020-10-19 21:48:26 +0200

    Empezado apartado bazkide html y creado php

---
+++ PHP/bazkideAreaBete.php
@@ -0,0 +1,5 @@
+<!-- Web atal honetan behar dugun informazioa aterako dugu -->
+    <!-- Hurrengo pelikularen argazkia eta izena -->
+    <!-- Bazkidearen izena eta puntuazioa -->
+    <!-- Bazkideen rankingaren lehenengo iruren izena eta puntuazioa -->
+    <!-- Bazkidea ondo egiten duenean azertikoaren puntuen gehiketa -->
