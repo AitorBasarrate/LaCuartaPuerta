4f4ed3bb41ea246f9b072c0fe5a487b4fd495954
oihanefpweb <oihanefpweb@gmail.com>
2020-10-21 13:47:08 +0200

    bazkide atalaren Izenburua eta argazkiaren datuak hartu(konprobau behar)

--- PHP/bazkideAreaBete.php
+++ PHP/bazkideAreaBete.php
@@ -1,5 +1,23 @@
 <!-- Web atal honetan behar dugun informazioa aterako dugu -->
-    <!-- Hurrengo pelikularen argazkia eta izena -->
-    <!-- Bazkidearen izena eta puntuazioa -->
-    <!-- Bazkideen rankingaren lehenengo iruren izena eta puntuazioa -->
-    <!-- Bazkidea ondo egiten duenean azertikoaren puntuen gehiketa -->
+   
+<?php try{
+        /* Hurrengo pelikularen argazkia eta izena  */
+            $izenburua;
+            $miConsulta = $miPDO->prepare("SELECT Izenburua,Argazkia FROM Filmak ORDER BY idPelikulak DESC LIMIT 1");
+            $miConsulta->execute();
+            $resultados = $miConsulta->fetchAll();
+            foreach ($resultados as $indice=>$actual){
+                $izenburua=$actual['Izenburua'];
+                $argazkia=$actual['pregunta'];
+              }
+              echo($izenburua+$argazkia);
+              
+        }catch( PDOException $Exception ) {
+            // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+            // String.
+            throw new MyDatabaseException( $Exception->getMessage( ) , $Exception->getCode( ) );
+        } 
+    /* Bazkidearen izena eta puntuazioa */
+    /* Bazkideen rankingaren lehenengo iruren izena eta puntuazioa */
+     /* Bazkidea ondo egiten duenean azertikoaren puntuen gehiketa */
+    ?>
