d20c5876b7ad568e9408c238fa6dfb2396299924
oihanefpweb <oihanefpweb@gmail.com>
2020-10-22 11:53:07 +0200

    Sozio Area
    Botoiak, diseinua,responsive, amaituta

---
+++ JS/bazkideArea.js
@@ -0,0 +1,6 @@
+function botoiHandiak(obj){
+    document.getElementById(obj).style.backgroundColor='#f5cb5c'
+}
+function botoiTxikiak(obj){
+    document.getElementById(obj).style.backgroundColor='#242423'
+}
