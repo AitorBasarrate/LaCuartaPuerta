9f938604fac5872c18f0cf6252a2b4bf1f77fcec
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2020-10-22 14:13:15 +0200

    Intento de subir a la bd

--- JS/leerCSV.js
+++ JS/leerCSV.js
@@ -7,8 +7,10 @@ $.ajax({
     url: '/Media/DB/movies_metadata.csv',
     dataType: 'text',
   }).done(csvIrakurri);
-{}
 
+      // sessionStorage.setItem('izenburuak', line.split(',')[0]);
+      // sessionStorage.setItem('generoa', line.split(',')[1]);
+      // sessionStorage.setItem('urtea', line.split(',')[7]);
   	
 function csvIrakurri(data) {
   /**
@@ -19,11 +21,25 @@ function csvIrakurri(data) {
  * nahi dudan datua irakurtzen dut.
  */
     allRows.forEach(line => {
-      console.log(line.split(',')[5]);
-      sessionStorage.setItem('izenburuak', line.split(',')[0]);
-      sessionStorage.setItem('generoa', line.split(',')[1]);
-      sessionStorage.setItem('urtea', line.split(',')[7]);
+      var izenburuak = (line.split(',')[0]);
+      var generoa = (line.split(',')[1]);
+      var rottenTomatoes = (line.split(',')[5]);
+      var urtea = (line.split(',')[7]);
+      
+      console.log(izenburuak);
+      const formData = new FormData;
+      const json = JSON.stringify(izenburuak);
+      formData.append('izenburuArray', json);
+
+      fetch('PHP/datuBaseaBete.php', {
+      method: 'POST',
+      body: formData
+      })
+      .then(res => res.text())
+      .then(data => console.log(data))
     });
+
+
     
     
   }
--- PHP/datuBaseaBete.php
+++ PHP/datuBaseaBete.php
@@ -1,17 +1,11 @@
 <?php
-include("/PHP/dbKonexioa.php");
-    if (isset($_SESSION['izenburuak'])) {
-        $izenburuak = $_SESSION['izenburuak'];
-        var_dump($izenburuak);
-    }else{
-        echo "que no funciona, que no"
-    }
-    $konexioa = new konexioa("localhost", "lacuartapuerta", "root", "")
+    var_dump($_POST);
+//     $konexioa = new konexioa("localhost", "lacuartapuerta", "root", "")
 
-    $hostPDO = "mysql:host=$konexioa->hostDB;dbname=$konexio->nombreDB;";
-    $miPDO = new PDO($hostPDO, $konexioa->usuarioDB, $konexioa->contrasenyaDB);
+//     $hostPDO = "mysql:host=$konexioa->hostDB;dbname=$konexio->nombreDB;";
+//     $miPDO = new PDO($hostPDO, $konexioa->usuarioDB, $konexioa->contrasenyaDB);
 
-    $miConsulta = $miPDO->prepare('INSERT INTO filmak (Izenburua) VALUES ("' + $izenburuak + '")');
+//     $miConsulta = $miPDO->prepare('INSERT INTO filmak (Izenburua) VALUES ("' + $izenburuak + '")');
 
-$miConsulta->execute();
+// $miConsulta->execute();
 ?>
