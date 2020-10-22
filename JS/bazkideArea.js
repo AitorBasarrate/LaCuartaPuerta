9deae241ccbc8a8f6f0c634af555ea2d3fcf83a5
oihanefpweb <oihanefpweb@gmail.com>
2020-10-22 13:24:01 +0200

    :)

--- JS/bazkideArea.js
+++ JS/bazkideArea.js
@@ -5,16 +5,5 @@ function botoiTxikiak(obj){
     document.getElementById(obj).style.backgroundColor='#242423'
 }
 function alertPosibilidades(obj){
-    
-    var confirm= alertify.confirm('Probando confirm','Confirmar solicitud?',null,null).set('labels', {ok:'Confirmar', cancel:'Cancelar'}); 	
- 
-    confirm.set({transition:'slide'});   	
-     
-    confirm.set('onok', function(){ //callbak al pulsar botón positivo
-            alertify.success('Has confirmado');
-    });
-     
-    confirm.set('oncancel', function(){ //callbak al pulsar botón negativo
-        alertify.error('Has Cancelado el dialog');
-    });
+   
 }
