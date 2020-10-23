7096f42bc0194219f74ebb3b64759a7b788c8f53
oihanefpweb <oihanefpweb@gmail.com>
2020-10-23 13:20:33 +0200

    qdfv
    dfvfee

--- JS/leerCSV.js
+++ JS/leerCSV.js
@@ -7,10 +7,8 @@ $.ajax({
     url: 'Media/DB/movies_metadata.csv',
     dataType: 'text',
   }).done(csvIrakurri);
+{}
 
-      // sessionStorage.setItem('izenburuak', line.split(',')[0]);
-      // sessionStorage.setItem('generoa', line.split(',')[1]);
-      // sessionStorage.setItem('urtea', line.split(',')[7]);
   	
 function csvIrakurri(data) {
   /**
@@ -22,6 +20,7 @@ function csvIrakurri(data) {
  * nahi dudan datua irakurtzen dut.
  */
     allRows.forEach(line => {
+<<<<<<< HEAD
       var izenburuak = (line.split(',')[0]);
       var generoa = (line.split(',')[1]);
       var rottenTomatoes = (line.split(',')[5]);
@@ -35,9 +34,13 @@ function csvIrakurri(data) {
       })
       .then(res => res.text())
       .then(data => console.log(data))
+=======
+      console.log(line.split(',')[5]);
+      sessionStorage.setItem('izenburuak', line.split(',')[0]);
+      sessionStorage.setItem('generoa', line.split(',')[1]);
+      sessionStorage.setItem('urtea', line.split(',')[7]);
+>>>>>>> parent of 9f93860... Intento de subir a la bd
     });
-
-
     
     
   }
