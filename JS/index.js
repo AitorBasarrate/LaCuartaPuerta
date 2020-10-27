5abd10b53ac2892090e7316151f547ae367ef449
oarranz <oihanefpweb@gmail.com>
2020-10-27 09:16:13 +0100

    Botones index
    Al pasar por el video o las peliculas se pone amarillo

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+function botoiHandiak(obj){
+    document.getElementById(obj).style.backgroundColor='#f5cb5c'
+
+
+}
+function botoiTxikiak(obj){
+    document.getElementById(obj).style.backgroundColor='#E8EDDF'
+}
