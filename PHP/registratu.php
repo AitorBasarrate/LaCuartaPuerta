4458c0e6bfcceda9ea1e6275ed7989f458cfe942
ik_2dw3 <oihanefpweb@gmail.com>
2020-11-05 08:40:02 +0100

    MiniArreglo por que no se actualiza en mi locla

--- PHP/registratu.php
+++ PHP/registratu.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
     include ("dbKonexioa.php");
 
     
     $kontsulta = $miPDO -> prepare('INSERT INTO erabiltzaile (ErabiltzaileIzena, Pasahitza, Bimenak) VALUE ()');
-?>
+?> -->
