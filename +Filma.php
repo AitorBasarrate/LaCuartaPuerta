1404fc0fa15dfe3634e86af9ad70cbc652d59233
Goizane <goizaneterreros@gmail.com>
2020-11-13 13:25:48 +0100

    Generoa gordetzen da, ez da izarrengatik egiten filtroa ???? begiratu

--- +Filma.php
+++ +Filma.php
@@ -15,6 +15,22 @@
                 if((isset($_POST['star']))) {
                     $izarrak=$_POST['star'];
                     echo $izarrak;
+
+                    // switch ($izarrak) {
+                    //     // https://stackoverflow.com/questions/12620564/radio-buttons-with-cookie
+
+                    //     case 1:
+                    //         $izar1 = 'checked="checked"';
+                    //     case 2:
+                    //         $izar2 = 'checked="checked"'; 
+                    //     case 3:
+                    //         $izar3 = 'checked="checked"';  
+                    //     case 4:
+                    //         $izar4 = 'checked="checked"';  
+                    //     case 5:
+                    //         $izar5 = 'checked="checked"';  
+                    // }
+
                 }
                  
             ?>
@@ -83,15 +99,15 @@
                 <div class="stars">
                     <h3>Balorazioa:</h3>
                     <!-- <form action=""> -->
-                        <input class="star star-5" id="star-5" type="radio" name="star" value="5"/>
+                        <input class="star star-5" id="star-5" type="radio" name="star" value="5" <?php echo ($izarrak== 5) ?  "checked" : "" ;  ?>/>
                         <label class="star star-5" for="star-5"></label>
-                        <input class="star star-4" id="star-4" type="radio" name="star" value="4"/>
+                        <input class="star star-4" id="star-4" type="radio" name="star" value="4" <?php echo ($izarrak== 4) ?  "checked" : "" ;  ?>/>
                         <label class="star star-4" for="star-4"></label>
-                        <input class="star star-3" id="star-3" type="radio" name="star" value="3"/>
+                        <input class="star star-3" id="star-3" type="radio" name="star" value="3" <?php echo ($izarrak== 3) ?  "checked" : "" ;  ?>/>
                         <label class="star star-3" for="star-3"></label>
-                        <input class="star star-2" id="star-2" type="radio" name="star" value="2"/>
+                        <input class="star star-2" id="star-2" type="radio" name="star" value="2" <?php echo ($izarrak== 2) ?  "checked" : "" ;  ?>/>
                         <label class="star star-2" for="star-2"></label>
-                        <input class="star star-1" id="star-1" type="radio" name="star"value="1"/>
+                        <input class="star star-1" id="star-1" type="radio" name="star"value="1" <?php echo ($izarrak== 1) ?  "checked" : "" ;  ?>/>
                         <label class="star star-1" for="star-1"></label>
                     <!-- </form> -->
                 </div>  
