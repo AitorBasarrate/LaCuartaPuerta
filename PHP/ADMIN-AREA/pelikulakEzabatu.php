ad8335c9a975e25bb09370ac4356e36f88021bee
oarranz <oihanefpweb@gmail.com>
2020-11-16 18:42:33 +0100

    AHORA SI

--- PHP/ADMIN-AREA/pelikulakEzabatu.php
+++ PHP/ADMIN-AREA/pelikulakEzabatu.php
@@ -6,25 +6,6 @@ if(isset($_POST['delete'])){
         try{ 
             include 'PHP/dbKonexioa.php';
             $idPelikula = $_POST['idPelikula'];
-            $miConsulta = $miPDO->prepare("SELECT FROM iruzkinak WHERE filmak_idPelikulak='$idPelikula'");
-            if ($miConsulta->execute()) {
-                $fila = $miConsulta->rowCount();
-                if($fila!=0){
-                 $miConsulta = $miPDO->prepare("DELETE FROM iruzkinak WHERE filmak_idPelikulak='$idPelikula'");
-                 $miConsulta->execute();
-                }
-            }
-
-            $miConsulta = $miPDO->prepare("SELECT FROM galderak WHERE idPelikulak='$idPelikula'");
-            if ($miConsulta->execute()) {
-                $fila = $miConsulta->rowCount();
-                if($fila!=0){
-                    $miConsulta = $miPDO->prepare("DELETE FROM galderak WHERE idPelikulak='$idPelikula'");
-                    $miConsulta->execute();
-                }
-            }
-           
-
             /* Erabiltzaileen ID izena eta puntuak aterako dugu */
             $miConsulta = $miPDO->prepare("DELETE FROM filmak WHERE idPelikulak='$idPelikula'");
             $miConsulta->execute();
